12665d066c163908738bcaad4ca98f791edad1ec
arnoldknott <knott@dtu.dk>
2024-11-29 14:21:29 +0100

    chore: changes the node_modules mount in svelte_revealjs

--- scripts/run_slides_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
-cd $REPO_ROOT_DIR
-docker compose -f compose.yml -f compose.test.yml build slides
-docker compose -f compose.yml -f compose.test.yml up
-docker compose -f compose.yml -f compose.test.yml down
