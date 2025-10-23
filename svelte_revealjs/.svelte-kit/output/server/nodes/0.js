a6d7d89ecf7cf1a12e26e19600ae6bdb3a1152f4
arnoldknott <knott@dtu.dk>
2025-10-23 14:24:51 +0200

    chore: removes unnceccessary svelte-kit files from repo

--- svelte_revealjs/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.d2565f77.js","_app/immutable/chunks/scheduler.e108d1fd.js","_app/immutable/chunks/index.7255a256.js"];
-export const stylesheets = ["_app/immutable/assets/0.31d16134.css"];
-export const fonts = [];
